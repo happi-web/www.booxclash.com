9813d3a9314c2ec2fff6e1e293da57b23ad50b8d
Chilongo Kondwani <chilongokon@gmail.com>
2025-05-09 19:38:45 +0200

    Changed on how the component was rendered

--- frontend/src/StudentDashboard/HandsOnLearning/asserts/Lessons/lessonComponentMap.ts
+++ frontend/src/StudentDashboard/HandsOnLearning/asserts/Lessons/lessonComponentMap.ts
@@ -1,10 +1,10 @@
 // src/components/lessonComponentMap.ts
 import NumberClassifier from "../Lessons/NumberClassifier";
-import FractionBuilder from "../Lessons/Fractions";
+import Fractions from "../Lessons/Fractions";
 // Add more components as needed
 
 export const lessonComponentMap: Record<string, React.ComponentType> = {
   NumberClassifier,
-  FractionBuilder,
+  Fractions,
   // "StringKey": ComponentReference
 };
